4807fac4a650d93727c872f72b8acfdf99de1253
wakaka1123 <overmarsdyf@163.com>
2022-11-19 13:11:41 -0800

    added a9

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,40 +1,46 @@
-import posts from "./tuits.js";
-let tuits = posts;
+import * as tuitsDao from '../tuits/tuits-dao.js';
+// import posts from "./tuits.js";
+// let tuits = posts;
 
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
   const newTuit = req.body;
-  newTuit._id = (new Date()).getTime()+'';
+  // newTuit._id = (new Date()).getTime()+'';
   newTuit.likes = 0;
   newTuit.dislikes = 0;
   newTuit.liked = false;
-  tuits.push(newTuit);
-  res.json(newTuit);
+  // tuits.push(newTuit);
+  const insertedTuit = await tuitsDao.createTuit(newTuit);
+  res.json(insertedTuit);
 }
-const findTuits  = (req, res) => {
+const findTuits  = async (req, res) => {
+  const tuits = await tuitsDao.findTuits();
   return res.json(tuits);
 }
 
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
   const tuitdIdToDelete = req.params.tid;
-  tuits = tuits.filter((t) =>
-      t._id !== tuitdIdToDelete);
-
-
-  res.sendStatus(200);
+  // tuits = tuits.filter((t) =>
+  //     t._id !== tuitdIdToDelete);
+  const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+  res.json(status);
 }
 
-const updateTuit = (req, res) => {
+const updateTuit = async (req, res) => {
   const tuitdIdToUpdate = req.params.tid;
-
   const updates = req.body;
-  const tuitIndex = tuits.findIndex((t) => t._id === tuitdIdToUpdate)
-  if(tuitIndex>=0) {
-        tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
 
-        res.json(tuits[tuitIndex])
-    }else {
-        res.send(404)
-  }
+  const status = await tuitsDao.updateTuit(tuitdIdToUpdate,updates);
+  // console.log(updates)
+  res.json(updates);
+
+  // const tuitIndex = tuits.findIndex((t) => t._id === tuitdIdToUpdate)
+  // if(tuitIndex>=0) {
+  //       tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
+  //
+  //       res.json(tuits[tuitIndex])
+  //   }else {
+  //       res.send(404)
+  // }
 }
 
 
